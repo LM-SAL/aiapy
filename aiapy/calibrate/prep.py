fix off-by-one bug in pixel space in the bounding box coordinates

aiapy/calibrate/prep.py
@@ -78,7 +78,7 @@ def register(smap, missing=None, order=3, use_scipy=False):
     center = np.floor(tempmap.meta['crpix1'])
     range_side = (center + np.array([-1, 1]) * smap.data.shape[0] / 2) * u.pix
     newmap = tempmap.submap(u.Quantity([range_side[0], range_side[0]]),
-                            u.Quantity([range_side[1], range_side[1]]))
+                            u.Quantity([range_side[1], range_side[1]]) - 1*u.pix)
 
     newmap.meta['r_sun'] = newmap.meta['rsun_obs'] / newmap.meta['cdelt1']
     newmap.meta['lvl_num'] = 1.5