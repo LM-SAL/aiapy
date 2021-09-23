b82b78a93958d7eb092b83cccb4a7385ffb16b8b
Nabil Freij <freij@baeri.org>
2021-09-23 15:33:38 -0700

    Back to export

--- examples/download_specific_data.py
+++ examples/download_specific_data.py
@@ -7,27 +7,37 @@ This example shows how to request a specific series of AIA images from the JSOC.
 
 We will be filtering the data we require by keywords and requesting short exposure images from a recent flare.
 
-Unfortunately, this can not be done using the sunpy downloader,
-`Fido <https://docs.sunpy.org/en/stable/api/sunpy.net.Fido.html>`__
+Unfortunately, this can not be done using the sunpy downloader `~sunpy.net.Fido`
 and instead we will use the `drms <https://docs.sunpy.org/projects/drms/en/stable/>`__ Python library directly.
 """
+
+import os
+from pathlib import Path
 
 import drms
 import matplotlib.pyplot as plt
 
 import astropy.units as u
-import sunpy.map as smap
-from astropy.utils.data import download_file
+import sunpy.map
 
 from aiapy.calibrate import correct_degradation, normalize_exposure, register, update_pointing
 from aiapy.calibrate.util import get_correction_table, get_pointing_table
+
+#####################################################
+# Exporting data from the JSOC requires registering your
+# email first. Please replace this with your email
+# address once you have registered.
+# See `this page <http://jsoc.stanford.edu/ajax/register_email.html>`__
+# for more details.
+
+jsoc_email = os.environ.get("JSOC_EMAIL")
 
 #####################################################
 # Our goal is to request data of a recent (of time of writing)
 # X-class flare. However, we will request the explanation of
 # the keywords we want from the JSOC.
 
-client = drms.Client()
+client = drms.Client(email=jsoc_email)
 keys = [
     "EXPTIME",
     "QUALITY",
@@ -101,18 +111,29 @@ print("Quick Comparison")
 print(results.reset_index(drop=True) == records.reset_index(drop=True))
 
 #####################################################
-# From here you can now fetch the data.
-# We will avoid doing an export request here and get
-# the files directly from the JSOC.
+# From here you can now request (export) the data.
+# This will download this specific subset of data to your
+# local machine when the export request has been completed.
+# Depending on the status of the JSOC, this might take a while.
+#
+# Please be aware the script will hold until the export is complete.
 
-# We use a helper function from astropy to download these files for us.
-files = [download_file(f"http://jsoc.stanford.edu{filename}", cache=True) for filename in filenames["image"]]
-level_1_maps = smap.Map(files)
+export = client.export(updated_qstr, method="url", protocol="fits")
+files = export.download(Path("~/sunpy/").expanduser().as_posix())
+
+#####################################################
+# With AIA files, it is possible to bypass the export stage.
+# We can manually construct the URLS of the the data.
+# Be aware that each file will have the same filename based on the URL.
+# You will have to then use your preferred downloader to download the files.
+
+urls = [f"http://jsoc.stanford.edu{filename}" for filename in filenames.image]
 
 #####################################################
 # Now we will "prep" the data with every feature of
 # `aiapy` and plot the data sequence using `sunpy`.
 
+level_1_maps = sunpy.map.Map(files.download.to_list())
 # We get the pointing table outside of the loop for the relevant time range.
 # Otherwise you're making a call to the JSOC every single time.
 pointing_table = get_pointing_table(level_1_maps[0].date-3*u.h, level_1_maps[-1].date+3*u.h)
@@ -126,7 +147,7 @@ for a_map in level_1_maps:
     map_degradation = correct_degradation(map_registered, correction_table=correction_table)
     map_normalized = normalize_exposure(map_degradation)
     level_15_maps.append(map_normalized)
-sequence = smap.Map(level_15_maps, sequence=True)
+sequence = sunpy.map.Map(level_15_maps, sequence=True)
 sequence.peek()
 
 plt.show()
