081ed6c47d997b13a3b7fc78c0c463378e38018c
Will Barnes <will.t.barnes@gmail.com>
2021-10-27 12:32:55 +0000

    Review wording of degradation example.

--- examples/correct_degradation.py
+++ examples/correct_degradation.py
@@ -27,20 +27,19 @@ quantity_support()
 # can correct for this by modeling the degradation over time and
 # then dividing the image intensity by this correction.
 #
-# First, let's fetch one 335 Å AIA observational metadata between 2010
-# and 2018, one every 90 days.
-# We choose the 335 Å channel because it has experienced
+# First, let's fetch some metadata for the 335 Å channel of AIA between 2010
+# and 2018 at a cadence of 30 days. We choose the 335 Å channel because it has experienced
 # significant degradation compared to the other EUV channels.
 results = Fido.search(
     a.Time('2010-06-01T00:00:00', '2021-06-01T00:00:00'),
-    a.Sample(90*u.day),
+    a.Sample(30*u.day),
     a.jsoc.Series.aia_lev1_euv_12s,
     a.jsoc.Wavelength(335*u.angstrom),
 )
 
 ###########################################################
-# We only need some of the metadata that was returned.
-# In this case, the date, mean, and max columns and nothing else.
+# We only need the date and mean intensity columns from the
+# metadata that was returned. We select those and nothing else.
 table = results["jsoc"].show('DATE__OBS', 'DATAMEAN')
 table['DATAMEAN'].unit = u.ct
 table['DATE_OBS'] = astropy.time.Time(table['DATE__OBS'], scale='utc')
@@ -49,19 +48,21 @@ del table['DATE__OBS']
 print(table)
 
 ###########################################################
-# Next, we can pass the date column to the `~aiapy.calibrate.correct_degradation`
-# function. Which calculates the time-dependent correction factor
-# based on the time and wavelength of the observation, divides the intensity
-# by the correction factor, and returns correction factor. For more details
-# on how the correction factor is calculated, see the documentation for the
+# Next, we pass the date column to the `~aiapy.calibrate.correct_degradation`
+# function. This function calculates the time-dependent correction factor
+# based on the time and wavelength of the observation.
+# We then divide the mean intensity by the correction factor to get the corrected intensity.
+For more details on how the correction factor is calculated, see the documentation for the
 # `~aiapy.calibrate.degradation` function.
 correction_factor = degradation(335*u.angstrom, table['DATE_OBS'])
 # This correction can be applied to a sunpy map as well.
 table['DATAMEAN_DEG'] = table['DATAMEAN'] / correction_factor
 
 ###########################################################
-# To showcase the effect of degradation, using the total
-# flux as a function of time is the best way to visualize it.
+# To understand the effect of the degradation and the correction factor, we
+# plot the corrected and uncorrected mean intensity as a function of time.
+# Note that the uncorrected intensity decreases monotonically over time
+# while the corrected intensity recovers to pre-2011 values in 2020.
 plt.plot(table['DATE_OBS'], table['DATAMEAN'], label='mean', marker='o')
 plt.plot(table['DATE_OBS'], table['DATAMEAN_DEG'], label='mean, corrected', marker='o')
 plt.title(f'{(335*u.angstrom).to_string(format="latex")} Channel Degradation')
