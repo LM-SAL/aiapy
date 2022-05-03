c5c08868bdbcef96352e00cbefc50389c40b9d5b
Nabil Freij <freij@baeri.org>
2022-05-03 09:56:01 -0700

    tweak test

--- aiapy/calibrate/tests/test_prep.py
+++ aiapy/calibrate/tests/test_prep.py
@@ -29,7 +29,9 @@ def test_register(aia_171_map, lvl_15_map):
     """
     # Check all of these for Map attributes and .meta values?
     # Check array shape
-    assert lvl_15_map.data.shape == aia_171_map.data.shape
+    # Due to fixes in sunpy 3.1.6, the shape can be different
+    # See https://github.com/sunpy/sunpy/pull/5803
+    assert lvl_15_map.data.shape in (aia_171_map.data.shape, (4094, 4094))
     # Check crpix values
     assert lvl_15_map.meta["crpix1"] == lvl_15_map.data.shape[1] / 2.0 + 0.5
     assert lvl_15_map.meta["crpix2"] == lvl_15_map.data.shape[0] / 2.0 + 0.5
