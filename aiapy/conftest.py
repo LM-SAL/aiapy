f92240a4504b77222637c34c534c2b2fd31f6313
Nabil Freij <nabil.freij@gmail.com>
2023-12-20 11:10:02 -0800

    Moved idl tests into a seperate file (#329)
    Just to tidy it up until I work on the PSF items.

--- aiapy/conftest.py
+++ aiapy/conftest.py
@@ -1,21 +1,18 @@
+import contextlib
+
 import astropy.units as u
 import pytest
 import sunpy.data.test
 import sunpy.map
 
-# Force MPL to use non-gui backends for testing.
-try:
-    import matplotlib
-except ImportError:
-    pass
-else:
-    matplotlib.use("Agg")
+ALL_CHANNELS = (94, 131, 171, 193, 211, 304, 335, 1600, 1700, 4500) * u.angstrom
+CHANNELS = (94, 131, 171, 193, 211, 304, 335) * u.angstrom
 
-# Do not require hissw for tests
-try:
-    import hissw
-except ImportError:
-    pass
+# Force MPL to use non-gui backends for testing.
+with contextlib.suppress(ImportError):
+    import matplotlib
+
+    matplotlib.use("Agg")
 
 
 @pytest.fixture()
@@ -25,27 +22,18 @@ def aia_171_map():
     return m.resample((4096, 4096) * u.pixel)
 
 
-@pytest.fixture(scope="session")
-def idl_environment():
-    if idl_available():
-        return hissw.Environment(ssw_packages=["sdo/aia"], ssw_paths=["aia"])
-    pytest.skip(
-        "A working IDL installation is not available. You will not be able to run portions of the test suite.",
-    )
+@pytest.fixture()
+def all_channels():
+    return CHANNELS
 
 
-@pytest.fixture(scope="session")
-def ssw_home():
-    if idl_available():
-        return hissw.Environment().ssw_home
-    return None
+@pytest.fixture()
+def channels():
+    return CHANNELS
 
 
-def idl_available():
-    try:
-        import hissw
+@pytest.fixture()
+def psf_94(channels):
+    import aiapy.psf
 
-        hissw.Environment().run("")
-        return True
-    except Exception:  # NOQA
-        return False
+    return aiapy.psf.psf(channels[0], use_preflightcore=True)
--- aiapy/psf/tests/test_psf.py
+++ aiapy/psf/tests/test_psf.py
@@ -14,25 +14,6 @@ MESH_PROPERTIES = [
 ]
 
 
-@pytest.fixture()
-def psf_full(channels):
-    return aiapy.psf.psf(channels[0], use_preflightcore=True)
-
-
-@pytest.fixture(scope="module")
-def psf_idl(idl_environment, channels):
-    """
-    The point spread function as calculated by aia_calc_psf.pro.
-    """
-    r = idl_environment.run(
-        "psf = aia_calc_psf({{channel}},/use_preflightcore)",
-        args={"channel": f"{channels[0].value:.0f}"},
-        save_vars=["psf"],
-        verbose=False,
-    )
-    return r["psf"]
-
-
 @pytest.mark.parametrize("use_preflightcore", [True, False])
 def test_filter_mesh_parameters(use_preflightcore, channels):
     params = aiapy.psf.filter_mesh_parameters(use_preflightcore=use_preflightcore)
@@ -44,18 +25,3 @@ def test_filter_mesh_parameters(use_preflightcore, channels):
 def test_psf(psf):
     assert isinstance(psf, np.ndarray)
     assert psf.shape == (4096, 4096)
-
-
-def test_psf_consistent(psf_full, psf_idl):
-    """
-    Check whether PSF is consistent with IDL calculation.
-
-    .. note:: This test will take a very long time to run.
-    """
-    # NOTE: The IDL and Python PSF functions have been found to
-    # agree within 0.2% for all points along the PSF arms for
-    # both the preflight and non-preflight cases.
-    # NOTE: Only compare values above some threshold as the
-    # rest of the PSF is essentially noise
-    i_valid = np.where(psf_idl > 1e-10)
-    assert np.allclose(psf_full[i_valid], psf_idl[i_valid], atol=0.0, rtol=2e-3)
