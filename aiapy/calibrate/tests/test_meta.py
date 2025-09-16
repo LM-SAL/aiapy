6a5a92bcd53a81bce5f55cb3f9e820ac343ef071
Will Barnes <will.t.barnes@gmail.com>
2025-09-16 15:56:06 -0400

    use actual L1 193 file for update_pointing test

--- aiapy/calibrate/tests/test_meta.py
+++ aiapy/calibrate/tests/test_meta.py
@@ -31,25 +31,25 @@ def mock_pointing_table():
         names=(
             "T_START",
             "T_STOP",
-            "A_171_X0",
-            "A_171_Y0",
-            "A_171_INSTROT",
-            "A_171_IMSCALE",
+            "A_193_X0",
+            "A_193_Y0",
+            "A_193_INSTROT",
+            "A_193_IMSCALE",
         ),
     )
 
 
 @pytest.mark.remote_data
-def test_fix_pointing(aia_171_map, pointing_table) -> None:
+def test_fix_pointing(aia_193_level1_map, pointing_table) -> None:
     # Smoke test to make sure expected keys are being updated
     keys = ["crpix1", "crpix2", "cdelt1", "cdelt2", "crota2", "x0_mp", "y0_mp"]
     # NOTE: This modification forces CROTA2 and CDELT{1,2} to be updated. Otherwise,
     # these keys are not modified since the existing metadata and the pointing table
     # for this time are the same.
     new_pointing_table = pointing_table.copy()
-    new_pointing_table["A_171_INSTROT"] = 0 * u.deg
-    new_pointing_table["A_171_IMSCALE"] = 1.2 * u.arcsec / u.pixel
-    aia_map_updated = update_pointing(aia_171_map, pointing_table=new_pointing_table)
+    new_pointing_table["A_193_INSTROT"] = 0 * u.deg
+    new_pointing_table["A_193_IMSCALE"] = 1.2 * u.arcsec / u.pixel
+    aia_map_updated = update_pointing(aia_193_level1_map, pointing_table=new_pointing_table)
     for k in keys:
         assert k in aia_map_updated.meta.modified_items
 
@@ -70,25 +70,29 @@ def test_fix_pointing(aia_171_map, pointing_table) -> None:
         (1.001, 1),
     ],
 )
-def test_update_pointing_accuracy(aia_171_map, pointing_table, t_delt_factor, expected_entry) -> None:
+def test_update_pointing_accuracy(aia_193_level1_map, pointing_table, t_delt_factor, expected_entry) -> None:
     t_start = pointing_table[0]["T_START"]
     t_delt = pointing_table[0]["T_STOP"] - t_start  # This is nearly always 3 hours
-    aia_171_map.meta["T_OBS"] = (t_start + t_delt * t_delt_factor).isot
-    aia_map_updated = update_pointing(aia_171_map, pointing_table=pointing_table)
-    assert aia_map_updated.reference_pixel.x == pointing_table[expected_entry]["A_171_X0"]
-    assert aia_map_updated.reference_pixel.y == pointing_table[expected_entry]["A_171_Y0"]
+    aia_193_level1_map.meta["T_OBS"] = (t_start + t_delt * t_delt_factor).isot
+    aia_map_updated = update_pointing(aia_193_level1_map, pointing_table=pointing_table)
+    assert aia_map_updated.reference_pixel.x == pointing_table[expected_entry]["A_193_X0"]
+    assert aia_map_updated.reference_pixel.y == pointing_table[expected_entry]["A_193_Y0"]
 
 
 @pytest.mark.remote_data
-def test_update_pointing_submap(aia_171_map, pointing_table) -> None:
+def test_update_pointing_submap(aia_193_level1_map, pointing_table) -> None:
     # Tests that submapping and update pointing are commutative
     # NOTE: Purposefully cropping in pixel space as cropping in world space
     # can lead to differences of 1 pixel due to the differing world-to-pixel
     # conversions because of the change in reference pixel.
     blc = (500, 1000) * u.pixel
     trc = (1000, 1200) * u.pixel
-    m_submap_pointing_update = update_pointing(aia_171_map.submap(blc, top_right=trc), pointing_table=pointing_table)
-    m_pointing_update_submap = update_pointing(aia_171_map, pointing_table=pointing_table).submap(blc, top_right=trc)
+    m_submap_pointing_update = update_pointing(
+        aia_193_level1_map.submap(blc, top_right=trc), pointing_table=pointing_table
+    )
+    m_pointing_update_submap = update_pointing(aia_193_level1_map, pointing_table=pointing_table).submap(
+        blc, top_right=trc
+    )
     assert u.allclose(
         u.Quantity(m_submap_pointing_update.reference_pixel),
         u.Quantity(m_pointing_update_submap.reference_pixel),
@@ -97,36 +101,36 @@ def test_update_pointing_submap(aia_171_map, pointing_table) -> None:
 
 
 @pytest.mark.remote_data
-def test_update_pointing_resampled_warning(aia_171_map, pointing_table) -> None:
-    m = aia_171_map.resample((512, 512) * u.pixel)
+def test_update_pointing_resampled_warning(aia_193_level1_map, pointing_table) -> None:
+    m = aia_193_level1_map.resample((512, 512) * u.pixel)
     with pytest.warns(AIApyUserWarning, match="Input map has plate scale"):
         update_pointing(m, pointing_table=pointing_table)
 
 
 @pytest.mark.remote_data
-def test_update_pointing_missing_mp_keys(aia_171_map, pointing_table) -> None:
-    new_meta = copy.deepcopy(aia_171_map.meta)
+def test_update_pointing_missing_mp_keys(aia_193_level1_map, pointing_table) -> None:
+    new_meta = copy.deepcopy(aia_193_level1_map.meta)
     del new_meta["x0_mp"]
-    m = aia_171_map._new_instance(aia_171_map.data, new_meta)
+    m = aia_193_level1_map._new_instance(aia_193_level1_map.data, new_meta)
     with pytest.warns(AIApyUserWarning, match="x0_mp and/or y0_mp keywords are missing."):
         update_pointing(m, pointing_table=pointing_table)
 
 
 @pytest.mark.remote_data
-def test_update_pointing_no_entry_raises_exception(aia_171_map, pointing_table) -> None:
+def test_update_pointing_no_entry_raises_exception(aia_193_level1_map, pointing_table) -> None:
     # This tests that an exception is thrown when entry corresponding to
     # T_START <= T_OBS < T_END cannot be found in the pointing table.
     # We explicitly set the T_OBS key
-    aia_171_map.meta["T_OBS"] = (aia_171_map.date - 30 * u.year).isot
+    aia_193_level1_map.meta["T_OBS"] = (aia_193_level1_map.date - 30 * u.year).isot
     with pytest.raises(IndexError, match="No valid entries for"):
-        update_pointing(aia_171_map, pointing_table=pointing_table)
+        update_pointing(aia_193_level1_map, pointing_table=pointing_table)
 
 
-def test_fix_pointing_missing_value(aia_171_map, mock_pointing_table) -> None:
+def test_fix_pointing_missing_value(aia_193_level1_map, mock_pointing_table) -> None:
     # Adjust map to a date we know has missing pointing information
-    aia_171_map.meta["date-obs"] = "2010-09-30T06:51:48.344"
-    aia_171_map.meta["t_obs"] = aia_171_map.meta["date-obs"]
+    aia_193_level1_map.meta["date-obs"] = "2010-09-30T06:51:48.344"
+    aia_193_level1_map.meta["t_obs"] = aia_193_level1_map.meta["date-obs"]
     with pytest.warns(AIApyUserWarning, match="Missing value in pointing table"):
-        aia_171_map_updated = update_pointing(aia_171_map, pointing_table=mock_pointing_table)
-    assert aia_171_map.meta["crpix1"] == aia_171_map_updated.meta["crpix1"]
-    assert aia_171_map.meta["crpix2"] == aia_171_map_updated.meta["crpix2"]
+        aia_171_map_updated = update_pointing(aia_193_level1_map, pointing_table=mock_pointing_table)
+    assert aia_193_level1_map.meta["crpix1"] == aia_171_map_updated.meta["crpix1"]
+    assert aia_193_level1_map.meta["crpix2"] == aia_171_map_updated.meta["crpix2"]
